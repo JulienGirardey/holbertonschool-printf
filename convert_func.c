342cb908de1520a044c8dd9de1fb7d61cb9111bf
JulienGirardey <juliengirardey2@gmail.com>
2025-03-25 14:30:43 +0100

    add function to print : string, int and char

--- convert_func.c
+++ convert_func.c
@@ -61,42 +61,6 @@ void print_int(va_list *arg)
 }
 
 /**
- * print_unsigned - function use to print an unsigned integer
- *
- * @arg: unsigned integer to print
- *
- * Return: Always void
- */
-void print_unsigned(va_list *arg)
-{
-	unsigned int n = va_arg(*arg, unsigned int);
-
-	unsigned int i = 0;
-
-	unsigned int digits[10];
-
-	unsigned int num = n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		while (num != 0)
-		{
-			digits[i] = num % 10;
-			num = num / 10;
-			i++;
-		}
-		while (i--)
-		{
-			_putchar(digits[i] + '0');
-		}
-	}
-}
-
-/**
 * print_string - function use to print a string
 *
 * @arg: string to print
